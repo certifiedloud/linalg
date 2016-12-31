e1bbe5f6272c49d651e825e6b24c899a00b6114a
Cody Heimberger <certifiedloud@gmail.com>
2016-12-31 16:14:20 -0700

    add magnitude and normalization

--- Vector.py
+++ Vector.py
@@ -1,4 +1,5 @@
 """ A python class to represent vectors """
+import math
 
 class Vector(object):
     def __init__(self, coordinates):
@@ -33,6 +34,18 @@ class Vector(object):
             newCoords.append(self.coordinates[i] * scalar)
         return Vector(newCoords)
 
+    def getMagnitude(self):
+        coordinatesSquared = [x**2 for x in self.coordinates]
+        return math.sqrt(sum(coordinatesSquared))
+
+    def getNormalization(self):
+        try:
+            return self.scalarMultiply(1/self.getMagnitude())
+
+
+        except ZeroDivisionError:
+            raise exception("Cannot normalize the zero vector")
+
 
 
 if __name__ == "__main__":
@@ -43,3 +56,5 @@ if __name__ == "__main__":
     print("Vec1 + Vec2  = {}".format(myVec1.add(myVec2)))
     print("Vec1 - Vec2  = {}".format(myVec1.subtract(myVec2)))
     print("Vec1 * 2  = {}".format(myVec1.scalarMultiply(2)))
+    print("Vec1 Magnitude = {}".format(myVec1.getMagnitude()))
+    print("Vec2 Magnitude = {}".format(myVec2.getMagnitude()))
