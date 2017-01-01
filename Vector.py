f3d55a0adbbe3dfc2d3686a569f619eff972e954
Cody Heimberger <certifiedloud@gmail.com>
2016-12-31 17:54:18 -0700

    trying operator overloading

--- Vector.py
+++ Vector.py
@@ -1,5 +1,6 @@
 """ A python class to represent vectors """
 import math
+import operator
 
 class Vector(object):
     def __init__(self, coordinates):
@@ -12,7 +13,7 @@ class Vector(object):
     def __eq__(self, v):
         return self.coordinates == v.coordinates
 
-    def add(self, v):
+    def __add__(self, v):
         if self.dimension != v.dimension:
             raise ValueError("Vector dimensions must be the same")
         newCoords = []
@@ -20,7 +21,7 @@ class Vector(object):
             newCoords.append(self.coordinates[i] + v.coordinates[i])
         return Vector(newCoords)
 
-    def subtract(self, v):
+    def __sub__(self, v):
         if self.dimension != v.dimension:
             raise ValueError("Vector dimensions must be the same")
         newCoords = []
@@ -28,33 +29,34 @@ class Vector(object):
             newCoords.append(self.coordinates[i] - v.coordinates[i])
         return Vector(newCoords)
 
-    def scalarMultiply(self, scalar):
-        newCoords = []
-        for i in range(self.dimension):
-            newCoords.append(self.coordinates[i] * scalar)
-        return Vector(newCoords)
+    def __mul__(self, other):
+        if type(other) is Vector:
+            theta = math.acos(operator.mul(self.getDirection(), other.getDirection()))
+            return operator.mul(self.getMagnitude(), other.getMagnitude(), math.cos(theta))
+        else:
+            newCoords = []
+            for i in range(self.dimension):
+                newCoords.append(operator.mul(self.coordinates[i], other))
+            return Vector(newCoords)
 
     def getMagnitude(self):
         coordinatesSquared = [x**2 for x in self.coordinates]
         return math.sqrt(sum(coordinatesSquared))
 
-    def getNormalization(self):
+    def getDirection(self):
         try:
-            return self.scalarMultiply(1/self.getMagnitude())
-
+            return self * ((1/self.getMagnitude()))
 
         except ZeroDivisionError:
             raise exception("Cannot normalize the zero vector")
-
-
 
 if __name__ == "__main__":
     myVec1 = Vector([1,2,3])
     myVec2 = Vector([4,5,6])
     print("Vec1 {}".format(myVec1))
     print("Vec2 {}".format(myVec2))
-    print("Vec1 + Vec2  = {}".format(myVec1.add(myVec2)))
-    print("Vec1 - Vec2  = {}".format(myVec1.subtract(myVec2)))
-    print("Vec1 * 2  = {}".format(myVec1.scalarMultiply(2)))
+    print("Vec1 + Vec2  = {}".format(myVec1 + myVec2))
+    print("Vec1 - Vec2  = {}".format(myVec1 - myVec2))
+    print("Vec1 * 2  = {}".format(myVec1 * 2))
     print("Vec1 Magnitude = {}".format(myVec1.getMagnitude()))
     print("Vec2 Magnitude = {}".format(myVec2.getMagnitude()))
